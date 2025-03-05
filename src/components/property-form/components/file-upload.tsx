5db101ffcc1f7cb0cf00ebcfddfde4b757d6155f
Hemat-ATOM <hematullahw@gmail.com>
2025-03-06 00:49:19 +0430

    Added poppins font instead of roboto

--- src/components/property-form/components/file-upload.tsx
+++ src/components/property-form/components/file-upload.tsx
@@ -1,4 +1,4 @@
-import { UploadCloud, X } from "lucide-react";
+import { ImagePlus, UploadCloud, X } from "lucide-react";
 import { ChangeEventHandler, useState } from "react";
 import { UseFormReturn } from "react-hook-form";
 import { IPropertyForm } from "..";
@@ -15,30 +15,38 @@ function FileUpload({ form }: { form: UseFormReturn<IPropertyForm> }) {
 
   const handleFileChange: ChangeEventHandler = (e) => {
     const fileList = (e.target as HTMLInputElement)?.files;
-    console.log(fileList)
+    console.log(fileList);
     if (fileList) {
       const files = Array.from(fileList);
 
       form.setValue("images", files);
 
-      setPreviews(prev => ([
-        ...prev, ...files.map(file => ({
+      setPreviews((prev) => [
+        ...prev,
+        ...files.map((file) => ({
           name: file.name,
           size: file.size,
           url: URL.createObjectURL(file),
-        }))
-      ]));
+        })),
+      ]);
     }
   };
 
   const handleDelete = (fileName: string) => {
-    setPreviews(prev => prev.filter(file => file.name !== fileName));
-    form.setValue("images", form.getValues("images").filter(file => (file as File).name !== fileName));
-  }
+    setPreviews((prev) => prev.filter((file) => file.name !== fileName));
+    form.setValue(
+      "images",
+      form
+        .getValues("images")
+        .filter((file) => (file as File).name !== fileName)
+    );
+  };
 
   return (
-    <div className="flex flex-col items-center gap-3 rounded-lg col-span-2">
-      <label className="max-w-xl mt-4 cursor-pointer bg-red-200 px-4 py-2 rounded-lg  w-full h-36 border-2 border-red-400 border-dashed flex flex-col gap-4 items-center justify-center">
+    <div className=" gap-3 rounded-lg col-span-2">
+      <h3>Upload the photos of your property (this help you to get more views).</h3>
+      <p className="text-sm text-gray-600">Photos should be in <span className="font-semibold">jpeg, png</span> or <span className="font-semibold">webp</span> format</p>
+      {/* <label className="max-w-xl mt-4 cursor-pointer bg-red-200 px-4 py-2 rounded-lg  w-full h-36 border-2 border-red-400 border-dashed flex flex-col gap-4 items-center justify-center">
         <UploadCloud size={42} color="#f82855" />
         <p className="text-sm text-gray-600">Upload multiple images (JPG, PNG, etc.)</p>
         <p className="text-sm text-gray-600">Maximum size for all files 30 MB</p>
@@ -49,18 +57,65 @@ function FileUpload({ form }: { form: UseFormReturn<IPropertyForm> }) {
           multiple
           onChange={handleFileChange}
         />
-      </label>
-      <div className="w-full ">
+      </label> */}
+      <div className="w-full grid grid-cols-3 gap-4">
+        <label className="max-w-xl mt-4 cursor-pointer bg-gray-200 px-4 py-2 rounded-lg  w-full h-36 border-2 border-gray-400 border-dashed flex flex-col gap-4 items-center justify-center">
+          <ImagePlus size={42} color="gray" />
+          <input
+            type="file"
+            className="hidden"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+          />
+        </label>
+        <label className="max-w-xl mt-4 cursor-pointer bg-gray-200 px-4 py-2 rounded-lg  w-full h-36 border-2 border-gray-400 border-dashed flex flex-col gap-4 items-center justify-center">
+          <ImagePlus size={42} color="gray" />
+          <input
+            type="file"
+            className="hidden"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+          />
+        </label>
+        <label className="max-w-xl mt-4 cursor-pointer bg-gray-200 px-4 py-2 rounded-lg  w-full h-36 border-2 border-gray-400 border-dashed flex flex-col gap-4 items-center justify-center">
+          <ImagePlus size={42} color="gray" />
+          <input
+            type="file"
+            className="hidden"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+          />
+        </label>
         {previews.length > 0 && (
-          <div className="grid sm:grid-cols-2 gap-2 mt-4">
+          <div className="w-full grid grid-cols-3 gap-4">
             {previews.map((file, index) => (
-              <div className="flex items-center w-full min-w-60 gap-2 border-2 p-2 rounded-xl hover:border-black transition-all duration-150">
-                <img key={index} src={file.url} alt={`Preview ${index}`} className="w-16 h-16 object-cover rounded-lg border-2" />
+              <div
+                className="flex items-center w-full min-w-60 gap-2 border-2 p-2 rounded-xl hover:border-black transition-all duration-150"
+                key={index}
+              >
+                <img
+                  key={index}
+                  src={file.url}
+                  alt={`Preview ${index}`}
+                  className="w-16 h-16 object-cover rounded-lg border-2"
+                />
                 <div>
                   <p className="font-medium">{file.name}</p>
-                  <p className="text-sm text-gray-500">{(file.size / (1024 ** 2)).toFixed(2)} MB</p>
+                  <p className="text-sm text-gray-500">
+                    {(file.size / 1024 ** 2).toFixed(2)} MB
+                  </p>
                 </div>
-                <Button size={"icon"} variant={"ghost"} className="ml-auto rounded-full" onClick={() => handleDelete(file.name)}><X /></Button>
+                <Button
+                  size={"icon"}
+                  variant={"ghost"}
+                  className="ml-auto rounded-full"
+                  onClick={() => handleDelete(file.name)}
+                >
+                  <X />
+                </Button>
               </div>
             ))}
           </div>
@@ -68,6 +123,6 @@ function FileUpload({ form }: { form: UseFormReturn<IPropertyForm> }) {
       </div>
     </div>
   );
-};
+}
 
 export default FileUpload;
